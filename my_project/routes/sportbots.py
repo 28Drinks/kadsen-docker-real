4040bc1a524da230d1ab14db2d130cfab14aa7fc
28Drinks <elias-w123@web.de>
2022-10-08 20:35:50 +0200

    special not working, display nada for now

--- my_project/routes/sportbots.py
+++ my_project/routes/sportbots.py
@@ -14,23 +14,28 @@ import matplotlib.pyplot as plt
 @app.route("/roi", methods=["GET", "POST"])
 def roi():
     if request.method == "POST":
-        text = request.form['text']
-        processed_text = "Sportsbot #" + text
-        bot = SportBot.query.filter(SportBot.name == processed_text).first()
+        try:
+            text = request.form['text']
+            processed_text = "Sportsbot #" + text
+            bot = SportBot.query.filter(SportBot.name == processed_text).first()
 
-        share_object = SportsShare.query.filter(SportsShare.sport == bot.sport).order_by(SportsShare.date.desc()).first()
+            share_object = SportsShare.query.filter(SportsShare.sport == bot.sport).order_by(SportsShare.date.desc()).first()
 
-        if bot.sport is not None:
-            base_share = share_object.base_share_value
-            bot_share = base_share * bot.sportshares
+            if bot.sport is not None:
+                base_share = share_object.base_share_value
+                bot_share = base_share * bot.sportshares
 
-            r_freebet = bot.freebet * 12 / 2
-            r_share = bot_share * 12
-            r_total = r_freebet + r_share
+                r_freebet = bot.freebet * 12 / 2
+                r_share = bot_share * 12
+                r_total = r_freebet + r_share
 
-            price = r_total * 2.5
+                price = r_total * 2.5
 
-            return render_template("roi-result.html", pagetitle="ROI", bot=bot, base_share=base_share,price=("%.2f" % price), bot_share=("%.2f" % bot_share),r_freebet=("%.2f" % r_freebet),r_share=("%.2f" % r_share),r_total=("%.2f" % r_total))
+                return render_template("roi-result.html", pagetitle="ROI", bot=bot, base_share=base_share,price=("%.2f" % price), bot_share=("%.2f" % bot_share),r_freebet=("%.2f" % r_freebet),r_share=("%.2f" % r_share),r_total=("%.2f" % r_total))
+
+        except:
+            return render_template("roi-result.html", bot=bot)
+
 
         else:
             return render_template("roi-result.html", bot=bot)
