15dd92f10f3c3ab09fd1c9d5bdae3707a4d16bd7
28Drinks <elias-w123@web.de>
2022-10-09 20:09:05 +0200

    daily / weekly toggle  at sportshare-today route added

--- my_project/routes/sports_share.py
+++ my_project/routes/sports_share.py
@@ -17,28 +17,43 @@ def share_today():
     today = today1.strftime('%Y-%m-%d')
     yesterday1 = datetime.now() - timedelta(days=1)
     yesterday = yesterday1.strftime('%Y-%m-%d')
+    previous_week1 = datetime.today() - timedelta(days=7)
+    previous_week = previous_week1.strftime('%Y-%m-%d')
 
     share_data_today = SportsShare.query.filter(SportsShare.date == today).order_by(SportsShare.combined_share_value.desc()).all()
     share_data_yesterday = SportsShare.query.filter(SportsShare.date == yesterday).order_by(SportsShare.combined_share_value.desc()).all()
+    share_data_previous_week = SportsShare.query.filter(SportsShare.date == previous_week).order_by(SportsShare.combined_share_value.desc()).all()
 
     change_in_dollar = {}
     change_in_percentage = {}
+    change_in_dollar_week = {}
+    change_in_percentage_week = {}
     base_share_value = {}
     share_quantity = {}
     bots_quantity = {}
-    for today,yesterday in zip(share_data_today, share_data_yesterday):
+    for today,yesterday,week in zip(share_data_today, share_data_yesterday,share_data_previous_week):
         # print(t.sport,t.value,y.sport,y.value)
         sport = today.sport
-        change_value = today.combined_share_value -yesterday.combined_share_value
+
+        # daily
+        change_value = today.combined_share_value - yesterday.combined_share_value
         change_percent = ((today.combined_share_value - yesterday.combined_share_value) / yesterday.combined_share_value) * 100
         change_in_percentage[sport] = round(change_percent, 2)
         change_in_dollar[sport] = round(change_value, 2)
+
+        # weekly
+        change_value_week = today.combined_share_value - week.combined_share_value
+        change_percent_week = ((today.combined_share_value - week.combined_share_value) / week.combined_share_value) * 100
+        change_in_percentage_week[sport] = round(change_percent_week, 2)
+        change_in_dollar_week[sport] = round(change_value_week, 2)
+
+        # bot/sport stats
         base_share_value[sport] = today.base_share_value
         share_quantity[sport] = today.share_quantity
         bots_quantity[sport] = today.bots_quantity
 
 
-    return render_template("share_today.html", page_title="Share Today", share_data_today=share_data_today, change_in_percentage=change_in_percentage, change_in_dollar=change_in_dollar, base_share_value=base_share_value, share_quantity=share_quantity, bots_quantity=bots_quantity)
+    return render_template("share_today.html", page_title="Share Today", share_data_today=share_data_today, change_in_percentage=change_in_percentage, change_in_dollar_week=change_in_dollar_week, change_in_percentage_week=change_in_percentage_week,change_in_dollar=change_in_dollar, base_share_value=base_share_value, share_quantity=share_quantity, bots_quantity=bots_quantity)
 
 
 # not refactored
